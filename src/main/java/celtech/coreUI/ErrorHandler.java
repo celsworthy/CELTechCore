cd06e2555f19449d46e35b0ccf6e93a934eb74f8
ianphudson <ian@cel-uk.com>
2014-06-03 12:02:57 +0100

    Suppress purge errors and send restart when out-of-sync purge error occurs

--- src/main/java/celtech/coreUI/ErrorHandler.java
+++ src/main/java/celtech/coreUI/ErrorHandler.java
@@ -80,17 +80,28 @@ public class ErrorHandler
             steno.trace("Requesting errors from printer");
             AckResponse errors = printer.transmitReportErrors();
             steno.trace("Errors are:\n" + errors.toString());
-            if (errors.isError() && !errors.isNozzleFlushNeededError())
+            if (errors.isError())
             {
-                if (errorDialog.isShowing() == false)
+                if (errors.isNozzleFlushNeededError())
                 {
-                    genericErrorPopupController.populateErrorList(errors);
-                    errorDialog.show();
                     steno.info("Resetting errors");
                     printer.transmitResetErrors();
                     if (printer.getPaused() == true)
                     {
                         printer.resumePrint();
+                    }
+                } else
+                {
+                    if (errorDialog.isShowing() == false)
+                    {
+                        genericErrorPopupController.populateErrorList(errors);
+                        errorDialog.show();
+                        steno.info("Resetting errors");
+                        printer.transmitResetErrors();
+                        if (printer.getPaused() == true)
+                        {
+                            printer.resumePrint();
+                        }
                     }
                 }
             }
