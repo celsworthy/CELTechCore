0fe4c5d14e8543233944a9f4d68c006cb3af202d
Tony Lynch <tony@cel-uk.com>
2014-08-07 09:23:59 +0100

    Start new look printer status component

---
+++ src/main/java/celtech/coreUI/components/printerstatus/PrinterComponent.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2014 CEL UK
+ */
+package celtech.coreUI.components.printerstatus;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author tony
+ */
+public class PrinterComponent extends Pane
+{
+
+    @FXML
+    private Label name;
+    
+    @FXML
+    private WhiteProgressBarComponent progressBar;   
+    
+    @FXML
+    private PrinterSVGComponent printerSVG;
+
+    public PrinterComponent()
+    {
+        URL fxml = getClass().getResource("/resources/printer.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxml);
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try
+        {
+            fxmlLoader.load();
+        } catch (IOException exception)
+        {
+            throw new RuntimeException(exception);
+        }
+        
+        initialise();
+    }
+
+    public void setName(String value)
+    {
+        nameTextProperty().set(value);
+    }
+
+    public StringProperty nameTextProperty()
+    {
+        return name.textProperty();
+    }
+
+    /**
+     * Initialise the component
+     */
+    private void initialise()
+    {
+        
+        progressBar.setStyle("-fx-progress-color: red;");
+        setWidth(50);
+        name.setTextFill(Color.WHITE);
+    }
+    
+    public void setProgress(double progress) {
+        progressBar.setProgress(progress);
+    }
+    
+    public void setColour(int color) {
+        String colourHexString = String.format("#%06X", color);
+        String style = "-fx-background-color: " + colourHexString + ";";
+        System.out.println("style is " + style);
+        setStyle(style);
+    }
+    
+    
+    public void setSize(int size) {
+//        setPrefWidth(size);
+        size = (int) (size * 0.9d);
+        setMinWidth(size);
+        setMaxWidth(size);
+//        setPrefHeight(size);
+        setMinHeight(size );
+        setMaxHeight(size);
+        printerSVG.setSize(size * 0.9);
+        progressBar.setLayoutX(size * (1 - 0.846) * 0.5);
+        progressBar.setLayoutY(size * 0.7);
+//        progressBar.setPrefWidth(size * 0.9);
+//        progressBar.setPrefHeight(size * 0.02);
+        progressBar.setControlWidth(size * 0.846);
+        progressBar.setControlHeight(size * 20.0 / 260.0);
+        
+        name.setStyle("-fx-font-size: 20;");
+        name.setLayoutX(size * 0.02);
+        name.setLayoutY(size * 0.85);
+    }
+}
---
+++ src/main/java/celtech/coreUI/components/printerstatus/PrinterSVGComponent.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 CEL UK
+ */
+
+package celtech.coreUI.components.printerstatus;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.SVGPath;
+
+/**
+ *
+ * @author tony
+ */
+public class PrinterSVGComponent extends Pane
+{
+    
+    @FXML Pane groupPane;
+    @FXML SVGPath p1;
+    @FXML SVGPath p2;
+    @FXML SVGPath p3;
+    @FXML SVGPath p4;
+    @FXML SVGPath p5;
+    @FXML SVGPath p6;
+    
+    Set<SVGPath> paths = new HashSet<>();
+    
+    public PrinterSVGComponent()
+    {
+        URL fxml = getClass().getResource("/resources/printerSVG.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxml);
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try
+        {
+            fxmlLoader.load();
+        } catch (IOException exception)
+        {
+            throw new RuntimeException(exception);
+        }
+        
+        paths.add(p1);
+        paths.add(p2);
+        paths.add(p3);
+        paths.add(p4);
+        paths.add(p5);
+        paths.add(p6);
+        
+        for (SVGPath path : paths)
+        {
+            path.setStyle("-fx-fill: white;");
+        }
+        
+    }
+    
+    public void setSize(double size) {
+        setScaleX(size/260.0);
+        setScaleY(size/260.0);
+    }
+    
+}
