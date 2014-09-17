fb68673fcf6c5a2c1ba4dda0bab03b3013f90c82
tony <tony@tony-Precision-WorkStation-T3500>
2014-09-17 16:24:16 +0100

    test autocrlf

---
+++ src/main/java/sandbox/TestCRLF.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2014 CEL UK
+ */
+package sandbox;
+
+/**
+ *
+ * @author tony
+ */
+public class TestCRLF
+{
+    /**
+     * This is some
+     * comments
+     * that 
+     * are over
+     * 1
+     * line
+     * .
+     */
+}
