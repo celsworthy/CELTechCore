b9976903e284e8ffd440bc92198563dd089b4c29
Tony <tony@cel-uk.com>
2014-11-06 15:10:27 +0000

    Preferences GUI WIP

--- src/main/java/celtech/coreUI/controllers/panels/PreferencesTopInsetPanelController.java
+++ src/main/java/celtech/coreUI/controllers/panels/PreferencesTopInsetPanelController.java
@@ -3,13 +3,17 @@
  */
 package celtech.coreUI.controllers.panels;
 
+import celtech.Lookup;
 import celtech.appManager.ApplicationMode;
 import celtech.appManager.ApplicationStatus;
+import celtech.coreUI.components.VerticalMenu;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Callable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -17,23 +21,41 @@ import javafx.fxml.Initializable;
  */
 public class PreferencesTopInsetPanelController implements Initializable
 {
-    
-        @FXML
+
+    @FXML
     void cancelPressed(ActionEvent event)
     {
-        ApplicationStatus.getInstance().modeProperty().set(ApplicationMode.STATUS);
+        ApplicationStatus.getInstance().returnToLastMode();
     }
 
     @FXML
     void backwardPressed(ActionEvent event)
     {
-        ApplicationStatus.getInstance().modeProperty().set(ApplicationMode.STATUS);
+        ApplicationStatus.getInstance().returnToLastMode();
     }
-    
+
+    @FXML
+    private VBox preferencesListContainer;
+
+    @FXML
+    private VerticalMenu preferencesMenu;
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        preferencesMenu.setTitle(Lookup.i18n("preferences.preferences"));
+        preferencesMenu.addItem("Environment", this::showEnvironmentPreferences);
+        preferencesMenu.addItem("Printing", this::showPrintingPreferences);
+    }
 
-    }    
+    private Object showEnvironmentPreferences()
+    {
+        return null;
+    }
     
+    private Object showPrintingPreferences()
+    {
+        return null;
+    }    
+
 }
