b8eb9ae1f6d58ec582b02c53c9aab47d46c813ac
ianphudson <ian@cel-uk.com>
2014-11-25 13:47:39 +0000

    Fixes ROB-228 - layout screen rotates when it shouldn't

--- src/main/java/celtech/coreUI/DisplayManager.java
+++ src/main/java/celtech/coreUI/DisplayManager.java
@@ -506,17 +506,17 @@ public class DisplayManager implements EventHandler<KeyEvent>
                 {
                     if (newTab == addPageTab)
                     {
-                        if (applicationStatus.getMode() != ApplicationMode.LAYOUT)
-                        {
-                            applicationStatus.setMode(ApplicationMode.LAYOUT);
-                        }
-
                         if (lastTab instanceof ProjectTab)
                         {
                             ((ProjectTab) lastTab).setMode(ApplicationMode.LAYOUT);
                         }
 
                         createAndAddNewProjectTab();
+
+                        if (applicationStatus.getMode() != ApplicationMode.LAYOUT)
+                        {
+                            applicationStatus.setMode(ApplicationMode.LAYOUT);
+                        }
                     } else if (newTab instanceof ProjectTab)
                     {
                         if (applicationStatus.getMode() != ApplicationMode.LAYOUT)
