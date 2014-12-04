2be794a59b691e6d789cc3e0c92a6bf6786a7dd2
Tony <tony@cel-uk.com>
2014-12-04 16:26:08 +0000

    Improve  calibration as per Chris's recommendations

--- src/test/java/celtech/printerControl/comms/CommandInterfaceTestPrinter.java
+++ src/test/java/celtech/printerControl/comms/CommandInterfaceTestPrinter.java
@@ -660,4 +660,10 @@ public class CommandInterfaceTestPrinter implements Printer
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public void openNozzleFullyExtra() throws PrinterException
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
