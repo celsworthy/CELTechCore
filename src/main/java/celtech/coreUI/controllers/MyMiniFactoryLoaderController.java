0c105ada861bf9cf78a8adb82ec39d82e0a99b66
Ian Hudson <ian@cel-uk.com>
2015-08-14 15:07:57 +0100

    Added check to see if My Mini Factory is up before allowing WebEngine to proceed - prevents failure to connect to the printer - temporary solution

--- src/main/java/celtech/coreUI/controllers/MyMiniFactoryLoaderController.java
+++ src/main/java/celtech/coreUI/controllers/MyMiniFactoryLoaderController.java
@@ -4,6 +4,7 @@ import celtech.Lookup;
 import celtech.appManager.ApplicationMode;
 import celtech.appManager.ApplicationStatus;
 import celtech.appManager.Project;
+import celtech.configuration.ApplicationConfiguration;
 import celtech.coreUI.DisplayManager;
 import celtech.coreUI.components.buttons.GraphicButtonWithLabel;
 import celtech.coreUI.visualisation.ModelLoader;
@@ -11,12 +12,17 @@ import celtech.utils.MyMiniFactoryLoadResult;
 import celtech.utils.MyMiniFactoryLoader;
 import celtech.web.AllCookiePolicy;
 import celtech.web.PersistentCookieStore;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.net.CookieStore;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
@@ -44,7 +50,7 @@ public class MyMiniFactoryLoaderController implements Initializable
 {
 
     private static final Stenographer steno = StenographerFactory.getStenographer(
-        MyMiniFactoryLoaderController.class.getName());
+            MyMiniFactoryLoaderController.class.getName());
 
     private WebEngine webEngine = null;
 
@@ -100,8 +106,12 @@ public class MyMiniFactoryLoaderController implements Initializable
     {
         addToProjectButton.disableProperty().bind(Bindings.equal("", fileDownloadLocation));
 
-        loadWebData();
+        boolean siteReachable = checkSiteIsReachable();
 
+        if (siteReachable)
+        {
+            loadWebData();
+        }
     }
 
     public void loadWebData()
@@ -121,42 +131,42 @@ public class MyMiniFactoryLoaderController implements Initializable
         webContentContainer.getChildren().addAll(webView);
 
         webEngine.load(myMiniFactoryURLString);
-        
-                webEngine.getLoadWorker().stateProperty().addListener(
-            (ObservableValue<? extends Worker.State> ov, Worker.State oldState, Worker.State newState) ->
-            {
-                switch (newState)
+
+        webEngine.getLoadWorker().stateProperty().addListener(
+                (ObservableValue<? extends Worker.State> ov, Worker.State oldState, Worker.State newState) ->
                 {
-                    case RUNNING:
-                        fileDownloadLocation.set("");
-                        DisplayManager.getInstance().startSpinning(webContentContainer);
-                        break;
-                    case SUCCEEDED:
-                        fileDownloadLocation.set("");
-                        DisplayManager.getInstance().stopSpinning();
-                        Object fileLinkFunction = webEngine
-                        .executeScript("window.autoMakerGetFileLink");
-                        if (fileLinkFunction instanceof JSObject)
-                        {
-                            fileDownloadLocation.set((String) webEngine
-                                .executeScript("window.autoMakerGetFileLink()"));
-                        }
-                        boolean okForBackwards = webEngine.getLocation().matches(".*\\/object\\/.*");
-                        forwardsPossible |= okForBackwards;
-                        boolean okForForwards = !okForBackwards && forwardsPossible;
-                        backwardButton.disableProperty().set(!okForBackwards);
-                        forwardButton.disableProperty().set(!okForForwards);
-                        break;
-                    case CANCELLED:
-                        fileDownloadLocation.set("");
-                        DisplayManager.getInstance().stopSpinning();
-                        break;
-                    case FAILED:
-                        fileDownloadLocation.set("");
-                        DisplayManager.getInstance().stopSpinning();
-                        break;
-                }
-            });
+                    switch (newState)
+                    {
+                        case RUNNING:
+                            fileDownloadLocation.set("");
+                            DisplayManager.getInstance().startSpinning(webContentContainer);
+                            break;
+                        case SUCCEEDED:
+                            fileDownloadLocation.set("");
+                            DisplayManager.getInstance().stopSpinning();
+                            Object fileLinkFunction = webEngine
+                            .executeScript("window.autoMakerGetFileLink");
+                            if (fileLinkFunction instanceof JSObject)
+                            {
+                                fileDownloadLocation.set((String) webEngine
+                                        .executeScript("window.autoMakerGetFileLink()"));
+                            }
+                            boolean okForBackwards = webEngine.getLocation().matches(".*\\/object\\/.*");
+                            forwardsPossible |= okForBackwards;
+                            boolean okForForwards = !okForBackwards && forwardsPossible;
+                            backwardButton.disableProperty().set(!okForBackwards);
+                            forwardButton.disableProperty().set(!okForForwards);
+                            break;
+                        case CANCELLED:
+                            fileDownloadLocation.set("");
+                            DisplayManager.getInstance().stopSpinning();
+                            break;
+                        case FAILED:
+                            fileDownloadLocation.set("");
+                            DisplayManager.getInstance().stopSpinning();
+                            break;
+                    }
+                });
     }
 
     private boolean alreadyDownloading = false;
@@ -176,7 +186,7 @@ public class MyMiniFactoryLoaderController implements Initializable
                 if (result.isSuccess())
                 {
                     modelLoader.loadExternalModels(Lookup.getSelectedProjectProperty().get(),
-                                                   result.getFilesToLoad());
+                            result.getFilesToLoad());
                 }
                 finishedWithEngines();
                 ApplicationStatus.getInstance().setMode(ApplicationMode.LAYOUT);
@@ -201,6 +211,42 @@ public class MyMiniFactoryLoaderController implements Initializable
     {
         alreadyDownloading = false;
         DisplayManager.getInstance().stopSpinning();
+    }
+
+    private boolean checkSiteIsReachable()
+    {
+        //This has been introduced since a lack of response from the far end seems to cause WebEngine to create lots and lots of threads
+        // culminating in out of memory errors and preventing the printer from connecting.
+        // Bit of a sticking plaster, but check to see if the site is reachable first...
+
+        boolean available = false;
+
+        try
+        {
+            URL obj = new URL(myMiniFactoryURLString);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setRequestMethod("GET");
+
+            //add request header
+            con.setConnectTimeout(500);
+            int responseCode = con.getResponseCode();
+
+            if (responseCode == 200
+                    && con.getContentLength() > 0)
+            {
+                available = true;
+            } else
+            {
+                steno.warning("My Mini Factory site unavailable");
+            }
+        } catch (IOException ex)
+        {
+            steno.error("Exception whilst attempting to contact My Mini Factory site");
+        }
+
+        return available;
     }
 
 }
