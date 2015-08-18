b4d5377e4c91a111e37a8de944cfd9917f7dddd3
Ian Hudson <ian@cel-uk.com>
2015-08-18 09:57:08 +0100

    Tweaked forced close value - if calculated nozzle position is less than 0.7 then treat this as 0

--- src/main/java/celtech/gcodetranslator/GCodeRoboxiser.java
+++ src/main/java/celtech/gcodetranslator/GCodeRoboxiser.java
@@ -1485,8 +1485,8 @@ public class GCodeRoboxiser extends GCodeRoboxisingEngine
                                         getMidPointPercent()
                                         / 100.0))));
                                 }
-                                if (compareDouble(currentNozzlePosition, 0, 10e-5)
-                                    == EQUAL
+                                if (compareDouble(currentNozzlePosition, 0, 0.07)
+                                    == MathUtils.LESS_THAN
                                     || currentNozzlePosition < 0)
                                 {
                                     currentNozzlePosition = 0;
