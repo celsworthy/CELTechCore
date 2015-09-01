d79a88e1ab41e3cdd7585d5225cd9651485248d0
Tony <tony@cel-uk.com>
2015-09-01 13:35:53 +0100

    Z cut WIP remove unused vertices

--- src/main/java/celtech/utils/threed/MeshUtils.java
+++ src/main/java/celtech/utils/threed/MeshUtils.java
@@ -5,28 +5,65 @@
  */
 package celtech.utils.threed;
 
+import java.util.HashSet;
+import java.util.Set;
 import javafx.scene.shape.TriangleMesh;
+
 
 /**
  *
  * @author alynch
  */
-public class MeshUtils {
+public class MeshUtils
+{
 
     /**
      * Remove vertices that are not used by any faces.
      */
-    static void removeUnusedVertices(TriangleMesh childMesh) {
-    
+    static void removeUnusedVertices(TriangleMesh childMesh)
+    {
+
+        // array of new vertex index for previous index
         int[] newVertexIndices = new int[childMesh.getPoints().size()];
-        
-        
-        for (int i = 0; i < childMesh.getFaces().size() / 2; i++) {
+        for (int i = 0; i < newVertexIndices.length; i++)
+        {
+            newVertexIndices[i] = -1;
+        }
+        float[] newPoints = new float[childMesh.getPoints().size()];
+        int nextNewPointIndex = 0;
+
+        for (int i = 0; i < childMesh.getFaces().size() / 2; i++) 
+        {
             int vertexIndex = childMesh.getFaces().get(i * 2);
-            
+            if (newVertexIndices[vertexIndex] == -1) {
+                newVertexIndices[vertexIndex] = nextNewPointIndex;
+                newPoints[nextNewPointIndex * 3] = childMesh.getPoints().get(vertexIndex * 3);
+                newPoints[nextNewPointIndex * 3 + 1] = childMesh.getPoints().get(vertexIndex * 3 + 1);
+                newPoints[nextNewPointIndex * 3 + 2] = childMesh.getPoints().get(vertexIndex * 3 + 2);
+                childMesh.getFaces().set(i * 2, nextNewPointIndex);
+                nextNewPointIndex++;
+            } else {
+                childMesh.getFaces().set(i * 2, newVertexIndices[vertexIndex]);
+            }    
         }
         
-    
+        childMesh.getPoints().clear();
+        childMesh.getPoints().addAll(newPoints, 0, nextNewPointIndex * 3);
     }
-    
+
+    /**
+     * Get the vertices being used by the mesh.
+     */
+    private static Set<Integer> getUsedVertices(TriangleMesh mesh)
+    {
+        Set<Integer> vertices = new HashSet<>();
+        for (int i = 0; i < mesh.getFaces().size() / 6; i++)
+        {
+            vertices.add(mesh.getFaces().get(i * 6));
+            vertices.add(mesh.getFaces().get(i * 6 + 2));
+            vertices.add(mesh.getFaces().get(i * 6 + 4));
+        }
+        return vertices;
+    }
+
 }
