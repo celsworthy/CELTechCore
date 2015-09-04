f0a770e6969df1daa753bd13bd38419826e937ce
Tony <tony@cel-uk.com>
2015-09-04 10:23:49 +0100

    Add mesh test utility, test mesh is orientable

---
+++ src/main/java/celtech/utils/threed/MeshDebug.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2015 CEL UK
+ */
+package celtech.utils.threed;
+
+import celtech.coreUI.visualisation.ApplicationMaterials;
+import celtech.modelcontrol.ModelContainer;
+import static celtech.utils.threed.MeshSeparator.addPointToMesh;
+import static celtech.utils.threed.MeshSeparator.setTextureAndSmoothing;
+import java.util.List;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.Sphere;
+import javafx.scene.shape.TriangleMesh;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+/**
+ *
+ * @author tony
+ */
+public class MeshDebug
+{
+    private static ModelContainer node;
+
+    private static void showFaceCentres(List<Integer> cutFaces, TriangleMesh mesh)
+    {
+        for (Integer faceIndex : cutFaces)
+        {
+            int v0 = mesh.getFaces().get(faceIndex * 6);
+            int v1 = mesh.getFaces().get(faceIndex * 6 + 2);
+            int v2 = mesh.getFaces().get(faceIndex * 6 + 4);
+            double x0 = mesh.getPoints().get(v0 * 3);
+            double y0 = mesh.getPoints().get(v0 * 3 + 1);
+            double z0 = mesh.getPoints().get(v0 * 3 + 2);
+            double x1 = mesh.getPoints().get(v1 * 3);
+            double y1 = mesh.getPoints().get(v1 * 3 + 1);
+            double z1 = mesh.getPoints().get(v1 * 3 + 2);
+            double x2 = mesh.getPoints().get(v2 * 3);
+            double y2 = mesh.getPoints().get(v2 * 3 + 1);
+            double z2 = mesh.getPoints().get(v2 * 3 + 2);
+            double xMin = Math.min(x0, Math.min(x1, x2));
+            double xMax = Math.max(x0, Math.max(x1, x2));
+            double x = (xMin + xMax) / 2;
+            double yMin = Math.min(y0, Math.min(y1, y2));
+            double yMax = Math.max(y0, Math.max(y1, y2));
+            double y = (yMin + yMax) / 2;
+            double zMin = Math.min(z0, Math.min(z1, z2));
+            double zMax = Math.max(z0, Math.max(z1, z2));
+            double z = (zMin + zMax) / 2;
+            Sphere sphere = new Sphere(0.5);
+            sphere.translateXProperty().set((x0 + x1 + x2) / 3.0);
+            sphere.translateYProperty().set((y0 + y1 + y2) / 3.0);
+            sphere.translateZProperty().set((z0 + z1 + z2) / 3.0);
+            sphere.setMaterial(ApplicationMaterials.getDefaultModelMaterial());
+            Text text = new Text(Integer.toString(faceIndex));
+            text.translateXProperty().set((x0 + x1 + x2) / 3.0);
+            text.translateYProperty().set((y0 + y1 + y2) / 3.0);
+            text.translateZProperty().set((z0 + z1 + z2) / 3.0);
+            Font font = new Font("Source Sans Pro Regular", 8);
+            text.setFont(font);
+            if (MeshDebug.node != null)
+            {
+                MeshDebug.node.addChildNode(sphere);
+                MeshDebug.node.addChildNode(text);
+            }
+        }
+    }
+
+    static void showFace(TriangleMesh mesh, int faceIndex)
+    {
+        TriangleMesh triangle = new TriangleMesh();
+        int[] vertices = new int[6];
+        vertices[0] = mesh.getFaces().get(faceIndex * 6);
+        vertices[2] = mesh.getFaces().get(faceIndex * 6 + 2);
+        vertices[4] = mesh.getFaces().get(faceIndex * 6 + 4);
+        triangle.getFaces().addAll(vertices);
+        addPointToMesh(mesh, vertices[0], triangle);
+        addPointToMesh(mesh, vertices[2], triangle);
+        addPointToMesh(mesh, vertices[4], triangle);
+        setTextureAndSmoothing(triangle, triangle.getFaces().size() / 6);
+        MeshView meshView = new MeshView(triangle);
+        meshView.setMaterial(ApplicationMaterials.pickedGCodeMaterial);
+        if (MeshDebug.node != null)
+        {
+            MeshDebug.node.addChildNode(meshView);
+        }
+    }
+
+    private static void showSphere(double x, double y, double z)
+    {
+        Sphere sphere = new Sphere(0.5);
+        sphere.translateXProperty().set(x);
+        sphere.translateYProperty().set(y);
+        sphere.translateZProperty().set(z);
+        sphere.setMaterial(ApplicationMaterials.getDefaultModelMaterial());
+        if (MeshDebug.node != null)
+        {
+            MeshDebug.node.addChildNode(sphere);
+        }
+    }
+
+    //    /**
+    //     * If a vertex lies on the cutting plane then perturb it to take it off the plane.
+    //     */
+    //    private static void perturbVerticesAtCutHeight(TriangleMesh mesh, double cutHeight,
+    //        BedToLocalConverter bedToLocalConverter)
+    //    {
+    //        for (int i = 0; i < mesh.getPoints().size(); i += 3)
+    //        {
+    //            Point3D pointInBed = bedToLocalConverter.localToBed(makePoint3D(mesh, i / 3));
+    //            if (Math.abs(pointInBed.getY() - cutHeight) < 1e-6) {
+    //                Point3D perturbedPointInBed = new Point3D(
+    //                    pointInBed.getX(),
+    //                    pointInBed.getY() + Math.random() / 1e3,
+    //                    pointInBed.getZ());
+    //                Point3D perturbedPointInLocal = bedToLocalConverter.bedToLocal(perturbedPointInBed);
+    //                mesh.getPoints().set(i, (float) perturbedPointInLocal.getX());
+    //                mesh.getPoints().set(i + 1, (float) perturbedPointInLocal.getY());
+    //                mesh.getPoints().set(i + 2, (float) perturbedPointInLocal.getZ());
+    //            }
+    //        }
+    //    }
+    public static void setDebuggingNode(ModelContainer node)
+    {
+        MeshDebug.node = node;
+    }
+
+    private static void showNewVertices(List<Integer> newVertices,
+        TriangleMesh mesh)
+    {
+        if (MeshDebug.node != null)
+        {
+            for (Integer newVertex : newVertices)
+            {
+                Sphere sphere = new Sphere(0.5);
+                sphere.translateXProperty().set(mesh.getPoints().get(newVertex * 3));
+                sphere.translateYProperty().set(mesh.getPoints().get(newVertex * 3 + 1));
+                sphere.translateZProperty().set(mesh.getPoints().get(newVertex * 3 + 2));
+                sphere.setMaterial(ApplicationMaterials.getOffBedModelMaterial());
+                MeshDebug.node.addChildNode(sphere);
+            }
+        }
+    }
+
+    private static void showIncomingMesh(TriangleMesh mesh)
+    {
+        System.out.println(mesh.getVertexFormat());
+        System.out.println(mesh.getVertexFormat().getVertexIndexSize());
+        System.out.println(mesh.getVertexFormat().getPointIndexOffset());
+        for (int i = 0; i < mesh.getPoints().size() / 3; i++)
+        {
+            System.out.println("point " + i + " is " + mesh.getPoints().get(i * 3) + " " +
+                mesh.getPoints().get(i * 3 + 1) + " " + mesh.getPoints().get(i * 3 + 2));
+            showSphere(mesh.getPoints().get(i * 3), mesh.getPoints().get(i * 3 + 1),
+                       mesh.getPoints().get(i * 3 + 2));
+        }
+        for (int i = 0; i < mesh.getFaces().size() / 6; i++)
+        {
+            System.out.println("face " + i + " is " + mesh.getFaces().get(i * 6) + " " +
+                mesh.getFaces().get(i * 6 + 2) + " " + mesh.getFaces().get(i * 6 + 4));
+        }
+    }
+    
+}
