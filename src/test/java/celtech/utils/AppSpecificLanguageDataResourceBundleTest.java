9ab07292817bb597d974b6b125b32d17cabafe29
Ian Hudson <ian@cel-uk.com>
2016-02-15 08:29:01 +0000

    Fixed language bundle test

--- src/test/java/celtech/utils/AppSpecificLanguageDataResourceBundleTest.java
+++ src/test/java/celtech/utils/AppSpecificLanguageDataResourceBundleTest.java
@@ -45,7 +45,7 @@ public class AppSpecificLanguageDataResourceBundleTest
     public void testLocaleUK_basedata()
     {
         Locale.setDefault(Locale.ENGLISH);
-        ResourceBundle bundle = ResourceBundle.getBundle("celtech.roboxbase.utils.LanguageDataResourceBundle", new UTF8Control());
+        ResourceBundle bundle = ResourceBundle.getBundle("celtech.roboxbase.utils.language.LanguageDataResourceBundle", new UTF8Control());
         assertEquals("Nozzle firmware control", bundle.getString("error.ERROR_B_POSITION_LOST"));
         assertEquals(1011, bundle.keySet().size());
     }
@@ -54,7 +54,7 @@ public class AppSpecificLanguageDataResourceBundleTest
     public void testLocaleUK_appdata()
     {
         Locale.setDefault(Locale.ENGLISH);
-        ResourceBundle bundle = ResourceBundle.getBundle("celtech.roboxbase.utils.LanguageDataResourceBundle", new UTF8Control());
+        ResourceBundle bundle = ResourceBundle.getBundle("celtech.roboxbase.utils.language.LanguageDataResourceBundle", new UTF8Control());
         assertEquals("Bed", bundle.getString("reelPanel.bed"));
         assertEquals(1011, bundle.keySet().size());
     }
