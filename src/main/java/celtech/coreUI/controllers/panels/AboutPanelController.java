296d8a4b3c0211955654ef7b5ed694ac021334c6
Ian Hudson <ian@cel-uk.com>
2016-03-16 15:31:19 +0000

    ROB-871 - copyright on about page updated. Reformatted about page to work in different resolutions and included auto-scaling of text

--- src/main/java/celtech/coreUI/controllers/panels/AboutPanelController.java
+++ src/main/java/celtech/coreUI/controllers/panels/AboutPanelController.java
@@ -1,8 +1,9 @@
 package celtech.coreUI.controllers.panels;
 
-import celtech.Lookup;
 import celtech.appManager.ApplicationMode;
 import celtech.appManager.ApplicationStatus;
+import celtech.configuration.ApplicationConfiguration;
+import celtech.coreUI.DisplayManager;
 import celtech.roboxbase.BaseLookup;
 import celtech.roboxbase.configuration.BaseConfiguration;
 import celtech.roboxbase.printerControl.model.Head;
@@ -12,12 +13,16 @@ import celtech.roboxbase.printerControl.model.PrinterListChangesListener;
 import celtech.roboxbase.printerControl.model.Reel;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -36,6 +41,39 @@ public class AboutPanelController implements Initializable, PrinterListChangesLi
 
     @FXML
     private Label version;
+
+        @FXML
+    private Label infoLabel;
+
+    @FXML
+    private Text bdLabel;
+
+    @FXML
+    private Text bdNames;
+
+    @FXML
+    private Text hwengLabel;
+
+    @FXML
+    private Text hwengNames;
+
+    @FXML
+    private Text swengLabel;
+
+    @FXML
+    private Text swengNames;
+
+    @FXML
+    private Text amTitleText1;
+
+    @FXML
+    private Text amTitleText2;
+
+    @FXML
+    private Text amTitleText3;
+
+    @FXML
+    private VBox logoBox;
 
     @FXML
     private void viewREADME(ActionEvent event)
@@ -74,6 +112,46 @@ public class AboutPanelController implements Initializable, PrinterListChangesLi
     {
         BaseLookup.getPrinterListChangesNotifier().addListener(this);
         version.setText(BaseConfiguration.getApplicationVersion());
+        
+        DisplayManager.getInstance().getDisplayScalingModeProperty().addListener(new ChangeListener<DisplayManager.DisplayScalingMode>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends DisplayManager.DisplayScalingMode> ov, DisplayManager.DisplayScalingMode t, DisplayManager.DisplayScalingMode scalingMode)
+            {
+                switch(scalingMode)
+                {
+                    case NORMAL:
+                        infoLabel.setStyle("-fx-font-size: 21px");
+                        hwengLabel.setStyle("-fx-font-size: 21px");
+                        hwengNames.setStyle("-fx-font-size: 21px");
+                        swengLabel.setStyle("-fx-font-size: 21px");
+                        swengNames.setStyle("-fx-font-size: 21px");
+                        bdLabel.setStyle("-fx-font-size: 21px");
+                        bdNames.setStyle("-fx-font-size: 21px");
+                        amTitleText1.setStyle("-fx-font-size: 100px");
+                        amTitleText2.setStyle("-fx-font-size: 100px");
+                        amTitleText3.setStyle("-fx-font-size: 14px");
+                        logoBox.setScaleX(1);
+                        logoBox.setScaleY(1);
+                        break;
+                    default:
+                        infoLabel.setStyle("-fx-font-size: 14px");
+                        hwengLabel.setStyle("-fx-font-size: 14px");
+                        hwengNames.setStyle("-fx-font-size: 14px");
+                        swengLabel.setStyle("-fx-font-size: 14px");
+                        swengNames.setStyle("-fx-font-size: 14px");
+                        bdLabel.setStyle("-fx-font-size: 14px");
+                        bdNames.setStyle("-fx-font-size: 14px");
+                        amTitleText1.setStyle("-fx-font-size: 70px");
+                        amTitleText2.setStyle("-fx-font-size: 70px");
+                        amTitleText3.setStyle("-fx-font-size: 10px");
+                        logoBox.setScaleX(0.8);
+                        logoBox.setScaleY(0.8);
+                        break;
+                        
+                }
+            }
+        });
     }
 
     @Override
