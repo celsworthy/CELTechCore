97a12b9e38e3a9e831aea0538f5a5d115ca491a4
Ian Hudson <ian@cel-uk.com>
2016-11-22 11:11:54 +0000

    Updates to Root connectivity

--- src/main/java/celtech/coreUI/controllers/panels/RootScannerPanelController.java
+++ src/main/java/celtech/coreUI/controllers/panels/RootScannerPanelController.java
@@ -1,36 +1,38 @@
 package celtech.coreUI.controllers.panels;
 
-import celtech.roboxbase.BaseLookup;
+import celtech.coreUI.DisplayManager;
 import celtech.roboxbase.comms.DetectedDevice;
 import celtech.roboxbase.comms.DetectedServer;
 import celtech.roboxbase.comms.DetectedServer.ServerStatus;
 import celtech.roboxbase.comms.DeviceDetectionListener;
 import celtech.roboxbase.comms.RemoteServerDetector;
+import celtech.roboxbase.comms.remote.Configuration;
 import celtech.roboxbase.configuration.CoreMemory;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.web.WebView;
 import libertysystems.stenographer.Stenographer;
 import libertysystems.stenographer.StenographerFactory;
-import org.controlsfx.glyphfont.Glyph;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.ContextMenuEvent;
 
 /**
  * FXML Controller class
@@ -48,138 +50,143 @@ public class RootScannerPanelController implements Initializable, MenuInnerPanel
     private Button scanButton;
 
     @FXML
-    private ListView<DetectedServer> scannedRoots;
+    private TableView<DetectedServer> scannedRoots;
 
     @FXML
-    void scanForRoots(ActionEvent event)
+    private WebView rootWebView;
+
+    @FXML
+    private TableColumn nameColumn;
+
+    @FXML
+    private TableColumn pinColumn;
+
+    @FXML
+    private TableColumn statusColumn;
+
+    private ObservableList<DetectedServer> currentServers = FXCollections.observableArrayList();
+    
+    private ContextMenu scannedRootContextMenu = new ContextMenu();
+
+//    class ServerListCell extends TableListCell<DetectedServer>
+//    {
+//
+//        private final BorderPane cellContainer;
+//        private final Label label;
+//        private final StackPane buttonContainer;
+//        private Circle statusBlob;
+//        private final Button connectButton;
+//        private final Button disconnectButton;
+//        private final Button upgradeButton;
+//        private DetectedServer detectedServer;
+//
+////        private final ChangeListener<ServerStatus> statusChangeListener = (ObservableValue<? extends ServerStatus> ov, ServerStatus t, ServerStatus t1) ->
+////        {
+////            switch (t1)
+////            {
+////                case NOT_THERE:
+////                    statusBlob.setFill(Color.RED);
+////                    break;
+////                case OK:
+////                    statusBlob.setFill(Color.GREEN);
+////                    break;
+////                case UNKNOWN:
+////                    statusBlob.setFill(Color.YELLOW);
+////                    break;
+////                case WRONG_VERSION:
+////                    statusBlob.setFill(Color.BLACK);
+////                    break;
+////            }
+////        };
+//
+//        public ServerListCell()
+//        {
+//            cellContainer = new BorderPane();
+//            cellContainer.setPrefWidth(USE_COMPUTED_SIZE);
+//
+////            HBox statusBlobHolder = new HBox();
+////            statusBlobHolder.alignmentProperty().set(Pos.CENTER);
+////            statusBlob = new Circle(10);
+////            statusBlob.setFill(Color.YELLOW);
+////            statusBlobHolder.getChildren().add(statusBlob);
+////            cellContainer.leftProperty().set(statusBlobHolder);
+//
+//            label = new Label();
+//            cellContainer.centerProperty().set(label);
+//
+//            connectButton = new Button("Connect");
+//            connectButton.setOnAction((ActionEvent t) ->
+//            {
+//                if (detectedServer != null)
+//                {
+//                    detectedServer.connect();
+//                }
+//            });
+//
+//            disconnectButton = new Button("Disconnect");
+//            disconnectButton.setVisible(false);
+//            disconnectButton.setOnAction((ActionEvent t) ->
+//            {
+//                if (detectedServer != null)
+//                {
+//                    detectedServer.disconnect();
+//                }
+//            });
+//
+//            upgradeButton = new Button("Upgrade");
+//            upgradeButton.setVisible(false);
+//            upgradeButton.setOnAction((ActionEvent t) ->
+//            {
+//                if (detectedServer != null)
+//                {
+//                }
+//            });
+//
+//            buttonContainer = new StackPane();
+//            buttonContainer.getChildren().addAll(connectButton, disconnectButton);
+//
+//            cellContainer.rightProperty().set(buttonContainer);
+//        }
+//
+//        @Override
+//        protected void updateItem(DetectedServer item, boolean empty)
+//        {
+//            DetectedServer oldItem = getItem();
+//
+////            if (oldItem != null)
+////            {
+////                oldItem.getServerStatusProperty().removeListener(statusChangeListener);
+////            }
+//
+//            super.updateItem(item, empty);
+//
+//            if (item != null && !empty)
+//            {
+//                detectedServer = item;
+//                setGraphic(cellContainer);
+//                label.setText(detectedServer.getName());
+//
+////                item.getServerStatusProperty().addListener(statusChangeListener);
+//
+//                connectButton.visibleProperty().unbind();
+//                connectButton.visibleProperty().bind(item.getServerStatusProperty().isNotEqualTo(ServerStatus.OK)
+//                        .and(item.getServerStatusProperty().isNotEqualTo(ServerStatus.WRONG_VERSION)));
+//
+//                disconnectButton.visibleProperty().unbind();
+//                disconnectButton.visibleProperty().bind(detectedServer.getServerStatusProperty().isEqualTo(ServerStatus.OK));
+//            } else
+//            {
+//                setGraphic(null);
+//                label.setText("");
+//            }
+//            detectedServer = item;
+//        }
+//    }
+    private void openWebViewOnRoot(DetectedServer server)
     {
-        scannedRoots.setItems(null);
-        Platform.runLater(() ->
-        {
-            List<DetectedServer> foundServers = remoteServerDetector.searchForServers();
-            if (foundServers.isEmpty())
-            {
-                scannedRoots.setItems(null);
-            } else
-            {
-                scannedRoots.setItems(FXCollections.observableArrayList(foundServers));
-            }
-        });
-    }
+        String url = "http://" + server.getAddress().getHostAddress() + ":" + Configuration.remotePort + "/";
 
-    class ServerListCell extends ListCell<DetectedServer>
-    {
-
-        private final BorderPane cellContainer;
-        private final Label label;
-        private final StackPane buttonContainer;
-        private Circle statusBlob;
-        private final Button connectButton;
-        private final Button disconnectButton;
-        private final Button upgradeButton;
-        private DetectedServer detectedServer;
-
-        private final ChangeListener<ServerStatus> statusChangeListener = (ObservableValue<? extends ServerStatus> ov, ServerStatus t, ServerStatus t1) ->
-        {
-            switch (t1)
-            {
-                case NOT_THERE:
-                    statusBlob.setFill(Color.RED);
-                    break;
-                case OK:
-                    statusBlob.setFill(Color.GREEN);
-                    break;
-                case UNKNOWN:
-                    statusBlob.setFill(Color.YELLOW);
-                    break;
-                case WRONG_VERSION:
-                    statusBlob.setFill(Color.BLACK);
-                    break;
-            }
-        };
-
-        public ServerListCell()
-        {
-            cellContainer = new BorderPane();
-            cellContainer.setPrefWidth(USE_COMPUTED_SIZE);
-
-            HBox statusBlobHolder = new HBox();
-            statusBlobHolder.alignmentProperty().set(Pos.CENTER);
-            statusBlob = new Circle(10);
-            statusBlob.setFill(Color.YELLOW);
-            statusBlobHolder.getChildren().add(statusBlob);
-            cellContainer.leftProperty().set(statusBlobHolder);
-
-            label = new Label();
-            cellContainer.centerProperty().set(label);
-
-            connectButton = new Button("Connect");
-            connectButton.setOnAction((ActionEvent t) ->
-            {
-                if (detectedServer != null)
-                {
-                    detectedServer.connect();
-                }
-            });
-
-            disconnectButton = new Button("Disconnect");
-            disconnectButton.setVisible(false);
-            disconnectButton.setOnAction((ActionEvent t) ->
-            {
-                if (detectedServer != null)
-                {
-                    detectedServer.disconnect();
-                }
-            });
-
-            upgradeButton = new Button("Upgrade");
-            upgradeButton.setVisible(false);
-            upgradeButton.setOnAction((ActionEvent t) ->
-            {
-                if (detectedServer != null)
-                {
-                }
-            });
-
-            buttonContainer = new StackPane();
-            buttonContainer.getChildren().addAll(connectButton, disconnectButton);
-
-            cellContainer.rightProperty().set(buttonContainer);
-        }
-
-        @Override
-        protected void updateItem(DetectedServer item, boolean empty)
-        {
-            DetectedServer oldItem = getItem();
-
-            if (oldItem != null)
-            {
-                oldItem.getServerStatusProperty().removeListener(statusChangeListener);
-            }
-
-            super.updateItem(item, empty);
-
-            if (item != null && !empty)
-            {
-                detectedServer = item;
-                setGraphic(cellContainer);
-                label.setText(detectedServer.getName() + "@" + detectedServer.getAddress().getHostAddress() + " v" + detectedServer.getVersion());
-
-                item.getServerStatusProperty().addListener(statusChangeListener);
-
-                connectButton.visibleProperty().unbind();
-                connectButton.visibleProperty().bind(item.getServerStatusProperty().isNotEqualTo(ServerStatus.OK)
-                        .and(item.getServerStatusProperty().isNotEqualTo(ServerStatus.WRONG_VERSION)));
-
-                disconnectButton.visibleProperty().unbind();
-                disconnectButton.visibleProperty().bind(detectedServer.getServerStatusProperty().isEqualTo(ServerStatus.OK));
-            } else
-            {
-                setGraphic(null);
-                label.setText("");
-            }
-            detectedServer = item;
-        }
+        rootWebView.getEngine().load(url);
     }
 
     /**
@@ -191,10 +198,104 @@ public class RootScannerPanelController implements Initializable, MenuInnerPanel
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        scannedRoots.setCellFactory(
-                (ListView<DetectedServer> param) -> new ServerListCell());
+//        scannedRoots.setCellFactory(
+//                (ListView<DetectedServer> param) -> new ServerListCell());
 
-        scannedRoots.setItems(FXCollections.observableArrayList(CoreMemory.getInstance().getActiveRoboxRoots()));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<DetectedServer, String>("name"));
+        pinColumn.setCellValueFactory(new PropertyValueFactory<DetectedServer, String>("pin"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<DetectedServer, ServerStatus>("serverStatus"));
+
+        scannedRoots.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        scannedRoots.setItems(currentServers);
+
+        scannedRoots.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<DetectedServer>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends DetectedServer> observable, DetectedServer oldValue, DetectedServer newValue)
+            {
+                if (newValue.getServerStatus() == ServerStatus.OK)
+                {
+                    openWebViewOnRoot(newValue);
+                }
+            }
+        });
+
+        String connectText = "Connect";
+        String disconnectText = "Disconnect";
+
+        MenuItem connectItem = new MenuItem(connectText);
+        MenuItem disconnectItem = new MenuItem(disconnectText);
+
+        connectItem.setOnAction((ActionEvent e) ->
+        {
+            scannedRoots.getSelectionModel().getSelectedItem().connect();
+        });
+        disconnectItem.setOnAction((ActionEvent e) ->
+        {
+            scannedRoots.getSelectionModel().getSelectedItem().disconnect();
+        });
+
+        scannedRootContextMenu.getItems().add(connectItem);
+        scannedRootContextMenu.getItems().add(disconnectItem);
+
+        scannedRoots.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>()
+        {
+
+            @Override
+            public void handle(ContextMenuEvent event)
+            {
+                scannedRootContextMenu.show(scannedRoots, event.getScreenX(), event.getScreenY());
+            }
+        });
+
+        Task<Void> scannerTask = new Task<Void>()
+        {
+            @Override
+            protected Void call() throws Exception
+            {
+                while (!isCancelled())
+                {
+                    List<DetectedServer> foundServers = remoteServerDetector.searchForServers();
+                    Platform.runLater(() ->
+                    {
+                        List<DetectedServer> serversToAdd = new ArrayList<>();
+                        List<DetectedServer> serversToRemove = new ArrayList<>();
+
+                        for (DetectedServer server : foundServers)
+                        {
+                            if (!currentServers.contains(server))
+                            {
+                                serversToAdd.add(server);
+                            }
+                        }
+
+                        for (DetectedServer server : currentServers)
+                        {
+                            if (!foundServers.contains(server))
+                            {
+                                serversToRemove.add(server);
+                            }
+                        }
+
+                        for (DetectedServer server : serversToAdd)
+                        {
+                            currentServers.add(server);
+                        }
+                        for (DetectedServer server : serversToRemove)
+                        {
+                            currentServers.remove(server);
+                        }
+                    });
+                }
+
+                return null;
+            }
+        };
+
+        Thread scannerThread = new Thread(scannerTask);
+        scannerThread.setDaemon(true);
+        scannerThread.start();
     }
 
     @Override
