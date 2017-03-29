29c02eb8056f1ae5b492beaa4b51be8b8616b98f
Ian Hudson <ian@cel-uk.com>
2017-03-29 15:23:23 +0100

    Added executeMacro method

--- src/test/java/celtech/printerControl/model/TestPrinter.java
+++ src/test/java/celtech/printerControl/model/TestPrinter.java
@@ -1217,4 +1217,10 @@ public class TestPrinter implements Printer
     {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public void executeMacroWithoutPurgeCheck(Macro macro) throws PrinterException
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
