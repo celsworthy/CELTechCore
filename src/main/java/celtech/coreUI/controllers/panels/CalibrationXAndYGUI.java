6917e5773a4e1f5767524f8c14ede67a4d60d7ec
George Salter <george@cel-uk.com>
2018-06-12 10:59:56 +0100

    Change calibrationStatus Label to HyperLinedLabel in nozzle calibration

--- src/main/java/celtech/coreUI/controllers/panels/CalibrationXAndYGUI.java
+++ src/main/java/celtech/coreUI/controllers/panels/CalibrationXAndYGUI.java
@@ -69,7 +69,7 @@ public class CalibrationXAndYGUI
     public void setState(CalibrationXAndYState state)
     {
         steno.debug("GUI going to state " + state);
-        controller.calibrationStatus.setText(state.getStepTitle());
+        controller.calibrationStatus.replaceText(state.getStepTitle());
         showAppropriateButtons(state);
         if (state.getDiagramName().isPresent())
         {
