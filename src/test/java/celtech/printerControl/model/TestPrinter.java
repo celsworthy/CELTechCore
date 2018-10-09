6fbf5f1a796ac5215dc30c5e47b20c854822b458
Tony Aldhous <tonyaldhous@cel-uk.com>
2018-10-09 12:40:45 +0100

    Update to test printer.

--- src/test/java/celtech/printerControl/model/TestPrinter.java
+++ src/test/java/celtech/printerControl/model/TestPrinter.java
@@ -1276,4 +1276,9 @@ public class TestPrinter implements Printer
     {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public void tidyProjectDirectories() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
