839a527600ba8595197687c63cad493d609d38fe
George Salter <george@cel-uk.com>
2019-03-19 10:14:51 +0000

    Add colons to labels with style class 'colon' now working for elements within ScrollPane

--- src/main/java/celtech/coreUI/controllers/panels/FXMLUtilities.java
+++ src/main/java/celtech/coreUI/controllers/panels/FXMLUtilities.java
@@ -6,6 +6,7 @@ package celtech.coreUI.controllers.panels;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.text.Text;
@@ -50,6 +51,16 @@ public class FXMLUtilities
                         {
                             addColonsToLabels((Parent) content);
                         }
+                    }
+                }
+                
+                if (node instanceof ScrollPane)
+                {
+                    ScrollPane scrollPane = (ScrollPane) node;
+                    Node content = scrollPane.getContent();
+                    if (content instanceof Parent)
+                    {
+                        addColonsToLabels((Parent) content);
                     }
                 }
             }
